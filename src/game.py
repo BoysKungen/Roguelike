821f463ab06d930ad478113aceadcee9550b2fcd
BoysKungen <alex.henryz@itggot.se>
2017-03-09 22:44:08 +0100

    Move graphics into seperate library

--- src/game.py
+++ src/game.py
@@ -1,5 +1,8 @@
+from src import gfx
 import curses
 import sys, traceback
+
+
 stdscr = None
 
 
@@ -12,45 +15,30 @@ class Game(object):
 		x,y = 0,0
 		while running:
 			curses.curs_set(0)
-			c = stdscr.getch()
+			c = gfx.scr().getch()
 			if c == curses.KEY_UP: 		y -= 1
 			elif c == curses.KEY_DOWN: 	y+=1
 			elif c == curses.KEY_LEFT: 	x -=1
 			elif c == curses.KEY_RIGHT: x +=1
 			elif c == ord('q'): running = False
 
+
+
 			if c != -1:
-				stdscr.clear()
-				stdscr.addch(y,x,"@")
+				gfx.scr().clear()
+				gfx.scr().addch(y,x,"@")
 			
 
 	#starts interactive session of the game with the player.
 	def play(self):
-		global stdscr
-		stdscr = curses.initscr()
-		curses.noecho()
-		curses.cbreak()
-		curses.curs_set(0)
-		stdscr.keypad(1)
-		stdscr.timeout(0)
-
-	
+		gfx.start()
 		try: 
 			self.step()
-		except:
-			curses.nocbreak()
-			stdscr.timeout(-1)
-			stdscr.keypad(0)
-			curses.echo()
-			curses.endwin()
-
-			print(traceback.format.exc())
-
+		except Exception as err:
+			gfx.stop()
+			#############################
+			print("Error: " + str(err))
 			sys.exit(-1)
 
-		curses.nocbreak()
-		curses.curs_set(1)
-		stdscr.timeout(-1)
-		stdscr.keypad(0)
-		curses.echo()
-		curses.endwin()
+
+		gfx.stop()
