7ff1ede8ca737661076686d847414e5203785347
BoysKungen <alex.henryz@itggot.se>
2017-03-10 01:35:40 +0100

    Added log Added world instead of whole cmd

--- src/game.py
+++ src/game.py
@@ -1,42 +1,38 @@
 from src import gfx
+from src import world
+
+
+
 import curses
 import sys, traceback
-
 
 stdscr = None
 
 
 class Game(object):
 	def __init__(self):
-		pass
+		self.world = world.World()
+		self.x,self.y = 0,0
 
-	def step(self):
-		running = True
-		x,y = 0,0
-		while running:
+	def step(self, c):
 			curses.curs_set(0)
-			c = gfx.scr().getch()
-			if c == curses.KEY_UP: 		y -= 1
-			elif c == curses.KEY_DOWN: 	y+=1
-			elif c == curses.KEY_LEFT: 	x -=1
-			elif c == curses.KEY_RIGHT: x +=1
-			elif c == ord('q'): running = False
-
-
-
-			if c != -1:
-				gfx.scr().clear()
-				gfx.scr().addch(y,x,"@")
-			
+			if c == "up": 		self.world.player_y -= 1
+			elif c == "down": 	self.world.player_y +=1
+			elif c == "left": 	self.world.player_x -=1
+			elif c == "right": 	self.world.player_x +=1
 
 	#starts interactive session of the game with the player.
 	def play(self):
 		gfx.start()
 		try: 
-			self.step()
+			running = True
+			while running:
+				c = gfx.get_input()
+				self.step(c)
+				if c == "q": running = False
+				self.world.draw()
 		except Exception as err:
 			gfx.stop()
-			#############################
 			print("Error: " + str(err))
 			sys.exit(-1)
 
